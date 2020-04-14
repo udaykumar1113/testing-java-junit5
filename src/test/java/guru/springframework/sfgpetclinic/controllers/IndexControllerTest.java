e91e896820d959f953904cc7a54476b86eba65dc
udayk <udaykumar1113@gmail.com>
2020-04-14 09:46:34 -0400

    JUnit timeout test

--- src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class IndexControllerTest {
@@ -29,5 +31,17 @@ class IndexControllerTest {
     @DisplayName("Exception Test of ValueNotFoundException")
     void oopsHandler() {
         assertThrows(ValueNotFoundException.class,() -> indexController.oopsHandler());
+    }
+
+    @Test
+    @DisplayName("Normal Timeout Test")
+    void testTimeout(){
+        assertTimeout(Duration.ofMillis(200),() -> { Thread.sleep(5000);});
+    }
+
+    @Test
+    @DisplayName("Preemptive Timeout Test")
+    void testPreemptiveTimeout(){
+        assertTimeoutPreemptively(Duration.ofMillis(200),() -> { Thread.sleep(5000);});
     }
 }
