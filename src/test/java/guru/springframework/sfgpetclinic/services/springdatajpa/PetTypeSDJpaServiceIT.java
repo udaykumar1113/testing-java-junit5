bc1b55747d4eae90b5a0e229fb545f6eeccc1669
udayk <udaykumar1113@gmail.com>
2020-05-10 10:18:45 -0400

    Maven fail safe plugin to run integration test

---
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/PetTypeSDJpaServiceIT.java
@@ -0,0 +1,28 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PetTypeSDJpaServiceIT {
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+}
